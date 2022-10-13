4b10be2a745eeb1dfd742e70fb352008150d8e36
ThomaxIgnition <thomax4blues@gmail.com>
2022-10-13 06:50:30 -0700

    broski changed it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,10 @@
-include <stdio.h>
-
+#include <stdio.h>
 /**
- *
- * * main - BROSKI
- *
- * *
- *
- * * Return: Always 0 (BROsuccess)
- *
- * */
+ * main - Broski
+ * Return: Always 0
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual
-			
-			puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

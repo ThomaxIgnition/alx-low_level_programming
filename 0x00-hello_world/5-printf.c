37ab86c8b1915f2dbefcd61ce5ec7fdbc456639a
ThomaxIgnition <thomax4blues@gmail.com>
2022-10-13 07:05:15 -0700

    Betty was the trick all along

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,25 +1,10 @@
 #include <stdio.h>
-
-
-
 /**
- *
- * * main - Print Broski
- *
- * *
- *
- * * Return: Always 0 (BROPassed)
- *
- * */
-
+ * main - BROSKI
+ * Return: Always 0 bro
+ */
 int main(void)
-
 {
-
-	printf("with proper grammar, but the outcome is a piece of
-
-			art,\n");
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }

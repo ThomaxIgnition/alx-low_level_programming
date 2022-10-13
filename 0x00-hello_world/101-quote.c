e735bd593f4fff6ba6f5ea196b3b1fdeb0b64853
ThomaxIgnition <thomax4blues@gmail.com>
2022-10-13 08:58:08 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Prints BROSKI
- * Return: Always 1 (Success)
+ * main - prints exactly and that piece of art is useful
+ *  Return: Always 0 (Sbroskisuccess)
  */
 int main(void)
-
 {
-
-	write(2, "and that piece of art is useful\" - Dora Korpar,
-
-			2015-10-19\n", 59);
-
-	return (1);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		return (1);
 }

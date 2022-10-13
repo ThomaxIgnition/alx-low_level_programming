c26229fcc7a9c0aeb3aa904f8e3ff5f78564cccb
ThomaxIgnition <thomax4blues@gmail.com>
2022-10-13 05:46:38 -0700

    bro did c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+nclude <stdio.h>
+
+
+
+/**
+ *
+ * * main - BROSKI
+ *
+ * *
+ *
+ * * Return: Always 0 (BROSuccess)
+ *
+ * */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual
+
+			puzzle");
+
+	return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * * main - Print Broski
+ *
+ * *
+ *
+ * * Return: Always 0 (BROPassed)
+ *
+ * */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of
+
+			art,\n");
+
+	return (0);
+
+}

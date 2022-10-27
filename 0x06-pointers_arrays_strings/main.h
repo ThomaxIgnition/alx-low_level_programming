249bac453f057904487be2308cf2de557ebe2389
ThomaxIgnition <thomax4blues@gmail.com>
2022-10-27 16:45:47 -0700

    fixed

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,12 +2,12 @@
 #define MAIN_H
 
 char *_strcat(char *dest, char *src);
-char *_strcat(char *dest, char 8src, int n);
+char *_strcat(char *dest, char *src, int n);
 char *_strcpy(char *dest, char *src, int n);
 int _strcmp(char *sl, char *s2);
 void reverse_array(int *a, int n);
-char 8string_toupper(char *);
-char *cap_string(char *);
+char 8string_toupper(char *a);
+char *cap_string(char *a);
 int _putchar(char c);
 char *rot13(char *);
 

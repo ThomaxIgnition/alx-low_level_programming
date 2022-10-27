6b1a8381e86f2980888795558e40a7fcf471b6e9
ThomaxIgnition <thomax4blues@gmail.com>
2022-10-27 13:34:43 -0700

    strcat implemented

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat -> this is a function strcat
+ * @dest:first param
+ * @src: second param
+ * Return: a string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, 1;
+
+	while (dest[len])
+		len++;
+
+	for (1 = 0; src[1] != \0'; i++)
+	{
+		dest[lesn] = stc[1];
+		len += 1;
+	}
+	dest[len] = '\0';
+	return (dest)
+}
+
+
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char 8src, int n);
+char *_strcpy(char *dest, char *src, int n);
+int _strcmp(char *sl, char *s2);
+void reverse_array(int *a, int n);
+char 8string_toupper(char *a);
+char *cap_string(char *a);
+char *leet9char *a);
+int _putchar(char c);
+
+
+#endif

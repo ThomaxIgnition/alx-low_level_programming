10b14be733dc774ef92a3fa0e7dfd4a6f6c7a2b0
ThomaxIgnition <thomax4blues@gmail>
2022-12-12 14:36:39 -0800

    varibles all done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ * main - prints the alphabet in lowercase.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar9'\n');
+
+	return (0);
+}

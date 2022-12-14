6e49b490d9840265813f659beb3d396b69d1b39b
ThomaxIgnition <thomax4blues@gmail>
2022-12-13 16:56:57 -0800

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,10 +11,11 @@ int main(void)
 {
 
 	_putchar('_');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
-	_putchar('n');
+	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');

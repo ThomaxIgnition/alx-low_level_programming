0b9ba9aa4e0343175eac31968a1e6a32d351a068
ThomaxIgnition <thomax4blues@gmail>
2022-12-13 23:38:36 -0800

    lastly

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
 
 void print_alphabet_x10(void)
 {
-	int  i;
-	char j;
+	int  count;
+	char letter;
 
-	for (i = 0; i < 10; i++)
+	while (count++ <= 9)
 	{
-		for (j = 'a'; j <= 'z' j++)
+		for (letter = 'a'; letter <= 'z' letter++)
 		{
-			_putchar(j);
+			_putchar(letter);
 		}
 
 		_putchar('\n');
